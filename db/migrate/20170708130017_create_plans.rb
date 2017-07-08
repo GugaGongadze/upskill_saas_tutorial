111261d980096806eee6a2a2829f332f641fe070
Guga Gongadze <ggong13@freeuni.edu.ge>
2017-07-08 13:15:02 +0000

    Set price to decimal, not string

--- db/migrate/20170708130017_create_plans.rb
+++ db/migrate/20170708130017_create_plans.rb
@@ -2,7 +2,7 @@ class CreatePlans < ActiveRecord::Migration[5.0]
   def change
     create_table :plans do |t|
       t.string :name
-      t.string :price
+      t.decimal :price
       t.timestamps
     end
   end

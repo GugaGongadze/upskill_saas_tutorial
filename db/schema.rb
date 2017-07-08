c6e4e33d04139c2e9c5632586c80a1eda9afde8e
Guga Gongadze <ggong13@freeuni.edu.ge>
2017-07-08 13:08:56 +0000

    Created new table for plans

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708085204) do
+ActiveRecord::Schema.define(version: 20170708130017) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.text     "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string   "name"
+    t.string   "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

f2f32ad585e3fbad49a6709279fdfa784675c439
Guga Gongadze <ggong13@freeuni.edu.ge>
2017-07-23 13:51:42 +0000

    Added avatar column to Profiles table.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,7 @@
 class Profile < ActiveRecorde::Base
   belongs_to :user
-  
+  has_attached_file :avatar,
+                    :styles => { :medium => "300x300>", :thumb => "100x100" },
+                    :default_url => "images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\z/
 end

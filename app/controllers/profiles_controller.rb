e2dd4eb84dab2cfd3fd6d3ab564736880806cb7d
Guga Gongadze <ggong13@freeuni.edu.ge>
2017-07-23 10:31:48 +0000

    Added profiles create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,4 +6,21 @@ class ProfilesController < ApplicationController
       # Render blank profile details form 
       @profile = Profile.new
     end
+    
+    # POST to /users/:user_id/profile/new
+    def create
+      @user = User.find( params[:user_id] )
+      @profile = @user.build_profile( profile_params )
+      if @profile.save
+        flash[:success] = "Profile updated!"
+        redirect_to root_path
+      else 
+        render action: :new
+      end
+    end
+    
+    private
+      def profile_params
+        params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+      end
 end
